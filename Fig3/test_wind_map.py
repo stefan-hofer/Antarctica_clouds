a943a354ec5977a600ea671b89f851cc3b19fc2f
Stefan Hofer <s.hofer@bristol.ac.uk>
2020-04-22 15:05:39 +0200

    Correct small spelling error

--- Fig3/test_wind_map.py
+++ Fig3/test_wind_map.py
@@ -24,6 +24,7 @@ ds_grid = xr.Dataset({'RIGNOT': (['y', 'x'], MAR_grid.RIGNOT.values),
 
 ds_wind = xr.Dataset(ds_atm[['UU', 'VV', 'UV']].sel(TIME='2009-10-14').isel(TIME=0),
                 coords={'lat': (('Y', 'X'), ds_grid.LAT), 'lon': (('Y', 'X'), ds_grid.LON)})
+ds_mpy = ds_wind.metpy.parse_cf()
 # Set extent of the map for plotting
 lat_min = -90
 lat_max = -60
@@ -39,9 +40,9 @@ axs.set_extent([lon_min, lon_max, lat_min, lat_max], ccrs.PlateCarree())
 ds_wind.UV.isel(ATMLAY=19).plot.pcolormesh('lon', 'lat', ax=axs, transform=ccrs.PlateCarree(),
                                                robust=True, cbar_kwargs={'shrink': 0.7})
 # regrid shape controls the density of the wind barbs
-axs.barbs(ds_mpy.X, ds_mpy.Y, ds_mpy.UU.values, ds_mpy.VV.values,
-          transform=ccrs.SouthPolarStereo(), regrid_shape=35, length=5, linewidth=1, alpha=0.8)
-
+axs.barbs(ds_mpy.X.values, ds_mpy.Y.values, ds_wind.UU.isel(ATMLAY=19).values, ds_wind.isel(ATMLAY=19).VV.values,
+          transform=ccrs.SouthPolarStereo(),regrid_shape=35, length=5, linewidth=1, alpha=0.8)
+# regrid_shape=35, length=5, linewidth=1, alpha=0.8
 # add the coastline
 axs.add_feature(cartopy.feature.COASTLINE.with_scale(
     '50m'), zorder=1, edgecolor='black')
