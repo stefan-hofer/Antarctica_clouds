fe358f1b13fecc83009f1133b27ad45e7afd9257
Stefan Hofer <shofer@mortarboard.uio.no>
2020-10-12 13:15:22 +0200

    WIP: microphysics plot

--- Fig4/2d_microph.py
+++ Fig4/2d_microph.py
@@ -20,6 +20,11 @@ file_str = '/projects/NS9600K/shofer/blowing_snow/MAR/3D_monthly/'
 file_str_nobs = '/projects/NS9600K/shofer/blowing_snow/MAR/3D_monthly_nDR/'
 file_str_zz = '/projects/NS9600K/shofer/blowing_snow/MAR/case_study_BS_2009/'
 
+# Wessel file folders
+file_str = '/uio/kant/geo-metos-u1/shofer/data/3D_monthly/'
+file_str_nobs = '/uio/kant/geo-metos-u1/shofer/data/3D_monthly_nDR/'
+file_str_zz = '/uio/kant/geo-metos-u1/shofer/data/MAR_ANT_35/'
+
 year_s = '2000-01-01'
 year_e = '2019-12-31'
 # Open the no driftig snow file
@@ -32,7 +37,9 @@ ds_bs_IWP = (xr.open_dataset(
 ds_bs_LWP = (xr.open_dataset(
     file_str + 'mon-CWP-MAR_ERA5-1979-2019.nc')).sel(TIME=slice(year_s, year_e)).mean(dim='TIME')
 
-
+for ds in [ds_bs_CC, ds_bs_COD, ds_bs_IWP, ds_bs_LWP]:
+    ds['X'] = ds['X'] * 1000
+    ds['Y'] = ds['Y'] * 1000
 # ========================================
 ds_nobs_CC = (xr.open_dataset(
     file_str_nobs + 'mon-CC-MAR_ERA5-1979-2019.nc')).sel(TIME=slice(year_s, year_e)).mean(dim='TIME')
@@ -42,7 +49,9 @@ ds_nobs_IWP = (xr.open_dataset(
     file_str_nobs + 'mon-IWP-MAR_ERA5-1979-2019.nc')).sel(TIME=slice(year_s, year_e)).mean(dim='TIME')
 ds_nobs_LWP = (xr.open_dataset(
     file_str_nobs + 'mon-CWP-MAR_ERA5-1979-2019.nc')).sel(TIME=slice(year_s, year_e)).mean(dim='TIME')
-
+for ds in [ds_nobs_CC, ds_nobs_COD, ds_nobs_IWP, ds_nobs_LWP]:
+    ds['X'] = ds['X'] * 1000
+    ds['Y'] = ds['Y'] * 1000
 # ============================================
 MAR_grid = xr.open_dataset(
     file_str_zz + 'MARcst-AN35km-176x148.cdf')
@@ -62,6 +71,10 @@ diff_IWP = (ds_bs_IWP - ds_nobs_IWP).rename(
     {'X': 'x', 'Y': 'y'}).isel(x=slice(5, -5), y=slice(5, -5))
 diff_LWP = (ds_bs_LWP - ds_nobs_LWP).rename(
     {'X': 'x', 'Y': 'y'}).isel(x=slice(5, -5), y=slice(5, -5))
+
+diff_weighted_LWP = diff_LWP.CWP - (diff_CC.CC * diff_LWP.CWP)
+diff_weighted_IWP = diff_IWP.IWP - (diff_CC.CC * diff_IWP.IWP)
+diff_weighted_COD = diff_COD.COD - (diff_CC.CC * diff_COD.COD)
 
 diff_CC['LAT'] = ds_grid.LAT
 diff_CC['LON'] = ds_grid.LON
@@ -87,6 +100,10 @@ ax2 = fig.add_subplot(spec2[0, 1], projection=proj)
 ax3 = fig.add_subplot(spec2[1, 0], projection=proj)
 ax4 = fig.add_subplot(spec2[1, 1], projection=proj)
 # PLOT EVERYTHING
+# (diff_CC.CC *100).plot.pcolormesh('x', 'y', transform=proj, ax=ax1, robust=True, cbar_kwargs={'label': r'$\Delta$ CC (%)$', 'shrink':0.7})
+# diff_weighted_COD.plot.pcolormesh('x', 'y', transform=proj, ax=ax2, robust=True, cbar_kwargs={'label':'r'$\Delta$ COD', 'shrink':0.7})
+# diff_weighted_LWP.plot.pcolormesh('x', 'y', transform=proj, ax=ax3, robust=True, cbar_kwargs={'label':'r'$\Delta$ LWP (g/kg)', 'shrink':0.7})
+# diff_weighted_IWP.plot.pcolormesh('x', 'y', transform=proj, ax=ax4, robust=True, cbar_kwargs={'label':'r'$\Delta$ IWP (g/kg)', 'shrink':0.7})
 
 
 ax = [ax1, ax2, ax3, ax4]
@@ -111,15 +128,15 @@ for i in range(4):
     ax[i].set_boundary(circle, transform=ax[i].transAxes)
 
 cmap = 'YlGnBu_r'
-cont = ax[0].pcolormesh(diff_CC['x']*1000, diff_CC['y']*1000,
+cont = ax[0].pcolormesh(diff_CC['x'], diff_CC['y'],
                         (diff_CC.CC)*100,
-                        transform=proj, cmap='RdBu_r')
-cont2 = ax[1].pcolormesh(diff_COD['x']*1000, diff_COD['y']*1000,
+                        transform=proj, cmap='Reds')
+cont2 = ax[1].pcolormesh(diff_COD['x'], diff_COD['y'],
                          diff_COD.COD,
                          transform=proj, cmap='RdBu_r')
-cont3 = ax[2].pcolormesh(diff_LWP['x']*1000, diff_LWP['y']*1000,
+cont3 = ax[2].pcolormesh(diff_LWP['x'], diff_LWP['y'],
                          diff_LWP.CWP, transform=proj, cmap='RdBu_r')
-cont4 = ax[3].pcolormesh(diff_IWP['x']*1000, diff_IWP['y']*1000,
+cont4 = ax[3].pcolormesh(diff_IWP['x'], diff_IWP['y'],
                          diff_IWP.IWP, transform=proj, cmap='RdBu_r')
 # cont2 = ax[1].pcolormesh(trend_CC['lon'], trend_CC['lat'],
 #                          trend_CC.slope*30, transform=ccrs.PlateCarree(), vmin=-15, vmax=15, cmap='RdBu_r')
@@ -130,6 +147,8 @@ for i in range(4):
     ax[i].set_title(names[i], fontsize=16)
     ax[i].text(0.04, 1.02, letters[i], fontsize=22, va='center', ha='center',
                transform=ax[i].transAxes, fontdict={'weight': 'bold'})
+
+
 # fig.canvas.draw()
 
 cb = fig.colorbar(cont, ax=ax[2], ticks=list(
@@ -143,6 +162,7 @@ fig.tight_layout()
 #                    orientation = 'horizontal', fraction = 0.13, pad = 0.01, shrink = 0.8)
 # cbar.set_label(
 #    'Average DJF cloud cover 2002-2015 (%)', fontsize=18)
-
+fig.savefig('/uio/kant/geo-metos-u1/shofer/data/microphysics.png',
+            format='PNG', dpi=300)
 fig.savefig('/projects/NS9600K/shofer/blowing_snow/microphysics.png',
             format='PNG', dpi=300)
