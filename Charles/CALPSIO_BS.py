4c1283da2a25aa2fbda39d050e2f38c5a7c7ff7a
stefan.hofer <stefan.hofer@geo.uio.no>
2022-05-05 18:51:01 +0200

    WIP: BS analysis

--- Charles/CALPSIO_BS.py
+++ Charles/CALPSIO_BS.py
@@ -1,36 +1,83 @@
-import h5py
+
 import numpy as np
-import xarray as xr
 from cmcrameri import cm
-import cartopy.crs as ccrs
 import cartopy.feature as feat
+import datetime as dt
+import xarray as xr
+import xesmf as xe
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import cartopy.crs as ccrs
+import pandas as pd
+import glob
 import matplotlib.path as mpath
 
-f = h5py.File(
-    'CAL_LID_L2_BlowingSnow_Antarctica-Standard-V1-00.2015-02.hdf5', 'r')
 
+directory = '/projects/NS9600K/shofer/blowing_snow/sat_data/CALIOP_blowing_snow/'
+ds = xr.open_mfdataset(
+    directory + 'CAL_LID_L2_BlowingSnow_Antarctica-Standard-V1*.nc')
+ann_mean = ds.mean(dim='time')
+apr_oct = ds["bs_freq"].sel(time=slice(2007 - 2015)).where((ds.time.dt.month >= 4) & (
+    ds.time.dt.month <= 10)).mean(dim='time')
 
-list(f.keys())
-dset = f["Snow_Fields"]
-dset["Blowing_Snow_Layer_Optical_Depth"].shape  # yields (7445905, 1)
+file_str_zz = '/projects/NS9600K/shofer/blowing_snow/MAR/case_study_BS_2009/'
+# ==========================================================================
+# CREATE the ICE MASK
+# =========================================================
 
-dset_geo = f["Geolocation_Fields"]
-anc = f["Ancillary_Fields"]
-meta = f['Metadata']
+MAR_grid = xr.open_dataset(
+    file_str_zz + 'MARcst-AN35km-176x148.cdf')
+ds_grid = xr.Dataset({'RIGNOT': (['y', 'x'], MAR_grid.RIGNOT.values),
+                      'SH': (['y', 'x'], MAR_grid.SH.values),
+                      'lat': (['y', 'x'], MAR_grid.LAT.values),
+                      'lon': (['y', 'x'], MAR_grid.LON.values),
+                      'ICE': (['y', 'x'], MAR_grid.ICE.values),
+                      'AIS': (['y', 'x'], MAR_grid.AIS.values),
+                      'SOL': (['y', 'x'], MAR_grid.SOL.values),
+                      'GROUND': (['y', 'x'], MAR_grid.GROUND.values),
+                      'AREA': (['y', 'x'], MAR_grid.AREA.values),
+                      'ROCK': (['y', 'x'], MAR_grid.ROCK.values)},
+                     coords={'x': (['x'], MAR_grid.X.values * 1000),
+                             'y': (['y'], MAR_grid.Y.values * 1000)})
 
-lats = np.arange(-89.5, 90.5, 1)
-lons = np.arange(-180, 180, 1)
+ais = ds_grid['AIS'].where(ds_grid)['AIS'] > 0  # Only AIS=1, other islands  =0
+# Ice where ICE mask >= 30% (ICE[0-100%], dividing by 100 in the next ligne)
+ice = ds_grid['ICE'].where(ds_grid['ICE'] > 30)
+# Combine ais + ice/100 * factor area for taking into account the projection
+ice_msk = (ais * ice * ds_grid['AREA'] / 100)
 
+grd = ds_grid['GROUND'].where(ds_grid['GROUND'] > 30)
+grd_msk = (ais * grd * ds_grid['AREA'] / 100)
 
-# extract the blowing snow frequency
-blow_freq = (dset["Snow_Blowing_Frequence_Grid"]
-             [0, :, :] / dset['Observation_Grid']) * 100
+lsm = (ds_grid['AIS'] < 1)
+ground = (ds_grid['GROUND'] * ds_grid['AIS'] > 30)
 
-ds = xr.Dataset({"bs_freq": (['lat', 'lon'], blow_freq)},
-                coords={'lat': (['lat'], lats),
-                        'lon': (['lon'], lons),
-                        'time': pd.date_range("2015-02-01", periods=1)})
+shf = (ds_grid['ICE'] / ds_grid['ICE']).where((ds_grid['ICE'] > 30) &
+                                              (ds_grid['GROUND'] < 50) & (ds_grid['ROCK'] < 30) & (ais > 0))
+shelf = (shf > 0)
 
+x2D, y2D = np.meshgrid(ds_grid['x'], ds_grid['y'])
+sh = ds_grid['SH']
+
+dh = (ds_grid['x'].values[0] - ds_grid['x'].values[1]) / 2.
+
+# ====================================================================================
+# ==============================================================================
+# Regridding operations
+# ==============================================================================
+# This creates the output grid, atm I think can be done with any variable
+# as long as lat lon grid is present
+ds_out = ds_grid
+# Can be any MAR input grid as long as lat lon is present (rename!)
+# REGRID MAR
+ds_in = ds
+regridder_MAR = xe.Regridder(ds_in, ds_out, 'bilinear', periodic=True)
+
+regridded_ds = regridder_MAR(ds)  # whole bs dataset regridded to MAR grid
+regridded_BS = regridder_MAR(ann_mean)
+regridded_BS_seasonal = regridder_MAR(apr_oct)
 # PLOT THE MAP
 plt.close('all')
 proj = ccrs.SouthPolarStereo()
@@ -39,6 +86,7 @@ fig, axs = plt.subplots(nrows=1, ncols=1, subplot_kw={
 # fig = plt.figure(figsize=(8, 8))
 # spec2 = gridspec.GridSpec(ncols=1, nrows=1, figure=fig)
 # ax1 = fig.add_subplot(spec2[0, 0], projection=proj)
+
 
 ax = [axs]
 for i in range(1):
@@ -60,13 +108,19 @@ for i in range(1):
 
     ax[i].set_boundary(circle, transform=ax[i].transAxes)
 
-ds['bs_freq'].plot.pcolormesh(
-    'lon', 'lat', transform=ccrs.PlateCarree(), ax=ax[0], cmap=cm.bilbao)
-axs.add_feature(feat.COASTLINE)
+cont = ax[0].pcolormesh(ds_grid['x'], ds_grid['y'],
+                        regridded_BS['bs_freq'].where(ais == 1), transform=proj, cmap=cm.bilbao, vmin=0, vmax=50)
+# axs.add_feature(feat.COASTLINE)
+xr.plot.contour(ds_grid.SOL, levels=1, colors='black',
+                linewidths=0.4, transform=proj, ax=ax[i])
+xr.plot.contour(ground, levels=1, colors='black', linewidths=0.4, ax=ax[i])
 
+cb = fig.colorbar(cont, ax=ax[0], ticks=list(
+    np.arange(0, 50, 10)), shrink=0.8)
 # cb = fig.colorbar(cont, ax=ax[0], ticks=list(
 #     np.arange(-30, 35, 10)), shrink=0.8, orientation='horizontal')
 # cb.set_label(r'$\Delta$ Snow Content (g/kg)', fontsize=16)
+
 
 fig.tight_layout()
 
---
+++ Charles/extract_CALIPSO_BS.py
@@ -0,0 +1,43 @@
+import h5py
+import numpy as np
+import xarray as xr
+from cmcrameri import cm
+import pandas as pd
+import cartopy.crs as ccrs
+import cartopy.feature as feat
+import matplotlib.path as mpath
+import glob
+
+directory = '/projects/NS9600K/shofer/blowing_snow/sat_data/CALIOP_blowing_snow/'
+files = sorted(glob.glob(
+    directory + 'CAL_LID_L2_BlowingSnow_Antarctica-Standard-V1-00*.hdf5'))
+
+for file in files:
+    f = h5py.File(file, 'r')
+    dset = f["Snow_Fields"]
+
+    # extract the blowing snow frequency
+    blow_freq = (dset["Snow_Blowing_Frequence_Grid"]
+                 [0, :, :] / dset['Observation_Grid']) * 100
+    lats = np.arange(-89.5, 90.5, 1)
+    lons = np.arange(-180, 180, 1)
+    time = file.split("/")[-1].split(".")[-2] + '-01'
+    print('The time is: {}'.format(time))
+
+    ds = xr.Dataset({"bs_freq": (['lat', 'lon'], blow_freq)},
+                    coords={'lat': (['lat'], lats),
+                            'lon': (['lon'], lons),
+                            'time': pd.date_range(time, periods=1)})
+
+    save_str = file.split(".")[0] + "." + time + ".nc"
+
+    ds.to_netcdf(save_str)
+
+
+list(f.keys())
+
+dset["Blowing_Snow_Layer_Optical_Depth"].shape  # yields (7445905, 1)
+
+dset_geo = f["Geolocation_Fields"]
+anc = f["Ancillary_Fields"]
+meta = f['Metadata']
